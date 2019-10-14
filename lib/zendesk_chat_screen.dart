b12b6023c84ca111a00268b08fb4a9b5c3e20bff
Eliseu Codinhoto <eliseucodinhoto@Eliseus-MacBook-Pro.local>
2019-10-14 15:12:21 -0300

    Add some custom settings

--- lib/zendesk_chat_screen.dart
+++ lib/zendesk_chat_screen.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 
+class AppBarSettings {
+  final String title;
+  final TextStyle textStyle;
+  final Color backgroundColor;
+
+  AppBarSettings(this.title, this.textStyle, this.backgroundColor);
+}
+
 class ZendeskChatScreen extends StatefulWidget {
+  final AppBarSettings appBarSettings;
+
+  const ZendeskChatScreen({Key key, this.appBarSettings}) : super(key: key);
   @override
   _ZendeskChatScreenState createState() => _ZendeskChatScreenState();
 }
 
 class _ZendeskChatScreenState extends State<ZendeskChatScreen> {
+  AppBarSettings get appBarSettings => widget.appBarSettings;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(
+          appBarSettings.title,
+          style: appBarSettings.textStyle,
+        ),
+        backgroundColor: appBarSettings.backgroundColor,
+      ),
     );
   }
 }
